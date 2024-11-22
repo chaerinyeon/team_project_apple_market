d892fd94dbc0680dbf70c421101648d392da1615
213 <hello-ryan@kakao.com>
2024-11-22 17:00:56 +0900

    fix: project files (#1)

---
+++ lib/pages/product_detail/product_detail_page.dart
@@ -0,0 +1,40 @@
+// product_list/screens/product_detail_screen.dart
+
+import 'package:flutter/material.dart';
+import 'package:team_project_apple_market/pages/product_list/product/product.dart';
+
+class ProductDetailPage extends StatelessWidget {
+  final Product product;
+
+  const ProductDetailPage({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.name),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              product.imageUrl,
+              height: 200,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 16),
+            Text('브랜드: ${product.brand.toString().split('.').last}',
+                style: const TextStyle(fontSize: 18)),
+            const SizedBox(height: 8),
+            Text('가격: ${product.price} 만원',
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
+    );
+  }
+}
